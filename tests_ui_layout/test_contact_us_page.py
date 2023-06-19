6bb4e71f851f5b1f0c7bf24a498dd340382daebe
Ahad-web <ahada405@gmail.com>
2023-06-19 20:55:53 +0500

    edited test contact

--- tests_ui_layout/test_contact_us_page.py
+++ tests_ui_layout/test_contact_us_page.py
@@ -1,17 +1,17 @@
-import time
-from playwright.sync_api import Playwright, sync_playwright
-import pytest
-from pom.contact_us_page import ContactUs
-
-@pytest.mark.skip(reason='Duplicated test')
-def test_submit_form(playwright: Playwright):
-    browser = playwright.chromium.launch(headless=False , slow_mo=500)
-    page = browser.new_page()
-    contact_page = ContactUs(page)
-    contact_page.navigate()
-    contact_page.submit_form("Ahad", "st7 block F", "testmail@gmail.com", "1233222", "test subj", "test messageasdasdasdasdasdasasdasdasdasdasdasasdasdasd" )
-    print("Success")
-
-with sync_playwright() as playwright:
-    test_submit_form(playwright)
-    # time.sleep(5)
+# import time
+# from playwright.sync_api import Playwright, sync_playwright
+# import pytest
+# from pom.contact_us_page import ContactUs
+#
+# # @pytest.mark.skip(reason='Duplicated test')
+# def test_submit_form(playwright: Playwright):
+#     browser = playwright.chromium.launch(headless=False , slow_mo=500)
+#     page = browser.new_page()
+#     contact_page = ContactUs(page)
+#     contact_page.navigate()
+#     contact_page.submit_form("Ahad", "st7 block F", "testmail@gmail.com", "1233222", "test subj", "test messageasdasdasdasdasdasasdasdasdasdasdasasdasdasd" )
+#     print("Success")
+#
+# with sync_playwright() as playwright:
+#     test_submit_form(playwright)
+#     # time.sleep(5)
